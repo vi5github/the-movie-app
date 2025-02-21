4b4af0aeff51288ee339960604a8dd685de74ca0
vi5github <varunnissrani@gmail.com>
2025-02-21 09:43:22 +0530

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { apiKey } from './constants';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+
+const router = createBrowserRouter(createRoutesFromElements(<Route>
+
+</Route>))
 
 const App = () => {
-  console.log(apiKey);
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
