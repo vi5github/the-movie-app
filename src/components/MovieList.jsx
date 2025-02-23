da0ac7bc035aee4fcc0cae9e911eaefb0aaafd15
vi5github <varunnissrani@gmail.com>
2025-02-23 13:54:12 +0530

    Api integrated

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const MovieList = () => {
+const MovieList = ({data}) => {
+  const {movieApiResponse} = data;
+  // console.log(movieApiResponse);
+  if(movieApiResponse && movieApiResponse.Response === 'False'){
+    return <h1>{movieApiResponse.Error || "No Result Found"}</h1>
+  }
+
   return (
-    <div>MovieList</div>
+    <>
+    {
+      movieApiResponse.Search.map(movie => (<p>
+        {movie.Title}
+      </p>))
+    }
+    </>
+    // <div>MovieList</div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import SearchForm from '../components/SearchForm'
 import MovieList from '../components/MovieList'
+import { apiKey } from '../constants';
+import axios from 'axios';
+import { useLoaderData } from 'react-router-dom';
 
-export function loader({request}){
+export async function loader({request}){
     const url = new URL(request.url);
     const searchTerm = url.searchParams.get("search");
-    return null;
+    const movieSearchEndpoint = `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`;
+    const response = await axios.get(movieSearchEndpoint);
+    // console.log(response)
+    return {movieApiResponse : response.data};
 }
 
 function Home() {
+  const data = useLoaderData();
   return (
     <div>
         HomePage
         <SearchForm/>
-        <MovieList />
+        <MovieList data={data}/>
     </div>
   )
 }
