b17f2936eedf4a4fbfd9918f5c7b47009cc04dcd
vi5github <varunnissrani@gmail.com>
2025-02-23 15:11:18 +0530

    laoding state handled

--- src/pages/MasterLayout.jsx
+++ src/pages/MasterLayout.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
+import { useNavigation } from 'react-router-dom';
 
 
 const MasterLayout = (props) => {
+  const navigation = useNavigation();
   return (
     <nav>
         <Link to="/">Movie Search</Link>
-        <Outlet/>
+        {navigation.state === 'loading' ? <h1> Loading... </h1> : <Outlet/>}
     </nav>
 
   )
